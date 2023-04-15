469030baa35692d15b5ab5531512a0b8c17ad68e
quentin natoly <quentin.natoly@gmail.com>
2023-04-15 15:38:17 +0200

    Code parfaitement fonctionnel avant 'voir plus btn'

--- ruby-presents/src/components/form.jsx
+++ ruby-presents/src/components/form.jsx
@@ -1,105 +1,113 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 import Axios from 'axios';
 
 const Form = () => {
+
   const [allPresents, setAllPresents] = useState([]);
   const [randomElements, setRandomElements] = useState([]);
   const [age, setAge] = useState('');
   const [budget, setBudget] = useState('');
-  const [responses, setResponses] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false);
 
-  useEffect(() => {
+
+
+  useEffect(() =>  {
     Axios.get('http://localhost:3001').then((response) => {
-      setAllPresents(response.data);
-      setIsLoaded(true);
+        setAllPresents(response.data)
+        console.log("Data received: ", allPresents)
     });
   }, []);
 
-  function filterElements(responses) {
-    const filteredElements = allPresents.filter((element) => {
+  // filtrer les cadeaux en fonction des réponses
+  const filterElements = (responses) => {
+    const filteredElements = allPresents.filter(element => {
       const age = parseInt(responses.age);
       const budget = parseInt(responses.budget);
-      return (
-        parseInt(element.age) <= age && parseInt(element.budget) <= budget
-      );
+      return parseInt(element.age) <= age && parseInt(element.budget) <= budget;
     });
+    // showMoreBtn.addEventListener("click", () => {
+    //   displayElements(filteredElements);
+    // });
     return filteredElements;
   }
 
-  function selectRandomElements(filteredElements) {
+  // Selectionner 3 cadeaux aléatoires dans la liste qui correspond aux criteres
+  const selectRandomElements = (filteredElements) => {
     if (filteredElements.length === 0) {
       console.log("Pas de cadeau pour toi, nique ta race héhé");
       return [];
     }
-    while (randomElements.length < 3 && filteredElements.length > 0) {
+    const newRandomElements = [];
+    while (newRandomElements.length < 3 && filteredElements.length > 0) {
       const randomIndex = Math.floor(Math.random() * filteredElements.length);
-      randomElements.push(filteredElements[randomIndex]);
+      newRandomElements.push(filteredElements[randomIndex]);
       filteredElements.splice(randomIndex, 1);
     }
-    return randomElements;
+    setRandomElements(newRandomElements);
+    return newRandomElements;
   }
-
+  
+  // Actualisation les données du formulaire
   const handleChange = (e) => {
     switch (e.target.id) {
-      case "age":
+      case 'age':
         setAge(e.target.value);
-        break;
-      case "budget":
+      break
+      case 'budget':
         setBudget(e.target.value);
-        break;
-      default:
-        return;
-    }
-  };
+      break
+      default: return;
+  }
+} 
 
-  const submit = async (event) => {
-    event.preventDefault();
-    // Vérifiez si les données ont été chargées avant le submit
-    if (isLoaded) {
-      const filteredElements = filterElements(responses);
-      const randomElements = selectRandomElements(filteredElements);
-      return randomElements;
-    } else {
-      console.log("Données non chargées");
-    }
-  };
+
+    // Récupération des valeurs des champs du formulaire
+    const submit = async (event) => {
+        event.preventDefault();
+        // Récupération des valeurs des champs du formulaire
+        const responses = { age, budget };
+        // Lancement des fonctions
+        const filteredElements = filterElements(responses);
+        const randomElements = selectRandomElements(filteredElements);
+    
+        setRandomElements(randomElements);
+    };
+    
 
   return (
     <>
-      <form className="login-form-container" onSubmit={submit}>
-        <div className="inputs">
-          {console.log(allPresents)}
-          <label htmlFor="budget">Quel budget ?</label>
-          <input
-            type="number"
-            id="budget"
-            name="budget"
-            onChange={handleChange}
-            required
-          ></input>
+        <form className='login-form-container' onSubmit={submit}>
+            <div className="inputs">
+                {console.log(allPresents)}
+                <label htmlFor="budget">Quel budget ?</label>
+                <input type="number" id="budget" name="budget" onChange={handleChange} required></input>
+        
+                <label htmlFor="age">Quel âge ?</label>
+                <input type="number" id="age" name="age" onChange={handleChange} required></input>
+        
+                {/* <label for="gender">Homme ou femme ?</label>
+                <select id="gender" name="gender">
+                <option value="homme">Homme</option>
+                <option value="femme">Femme</option>
+                </select>
+        
+                <label for="interests">Centres d'intérêts :</label>
+                <textarea id="interests" name="interests"></textarea>
+        
+                <label for="hobbies">Hobbies :</label>
+                <textarea id="hobbies" name="hobbies"></textarea> */}
+            </div>
+            <button type="submit">Envoyer</button>
+        </form>
 
-          <label htmlFor="age">Quel âge ?</label>
-          <input
-            type="number"
-            id="age"
-            name="age"
-            onChange={handleChange}
-            required
-          ></input>
+        <div>
+            {console.log(randomElements)} 
+            {randomElements.map((present) =>
+                <a href={present.lien} key={present.id}>
+                    <img src={present.image} alt='imag'></img>
+                    <p>{present.name}</p>
+                </a>
+            )}
         </div>
-        <button type="submit">Envoyer</button>
-      </form>
-
-      <div>
-        {console.log(randomElements)}
-        {randomElements.map((present) => (
-          <a href={present.lien}>
-            <img src={present.image} alt="imag"></img>
-            <p>{present.name}</p>
-          </a>
-        ))}
-      </div>
     </>
   )
 }
