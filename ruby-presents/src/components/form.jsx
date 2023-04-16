5b2e46c7df82cfd9edf5f2651052f90c998a58e4
quentin natoly <quentin.natoly@gmail.com>
2023-04-16 22:20:23 +0200

    workflow suppe

--- ruby-presents/src/components/form.jsx
+++ ruby-presents/src/components/form.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 import "../Css/form.css"
+import TypeMessage from "./TypeMessage";
+
 import Axios from 'axios';
 
 const Form = () => {
@@ -11,6 +13,14 @@ const Form = () => {
   const [hobbies, sethobbies] = useState([]);
   const [budget, setBudget] = useState('');
   const [path, setPath] = useState('');
+
+
+
+
+  // const [isTyping, setIsTyping] = useState(false);
+  const [firstMessage, setFirstMessage] = useState('');
+  const [secondMessage, setSecondMessage] = useState('');
+  const [thirdMessage, setThirdMessage] = useState('');
   
   // const [randomElements, setRandomElements] = useState([]);
   // const [filteredElements, setFilteredElements] = useState([]);
@@ -27,6 +37,15 @@ const Form = () => {
         setAllPresents(response.data)
         console.log("Data received: ", allPresents)
     });
+    setTimeout(() => {
+      setFirstMessage("Bonjour, je m'apelle Ruby je viens t'aider à trouver un cadeau");
+    }, 1000);
+    setTimeout(() => {
+      setSecondMessage("C'est très simple tu à juste à suivre mes indications et à répondre aux questions");
+    }, 1000);
+    setTimeout(() => {
+      setThirdMessage("Tout d'abord, la personne à qui tu veux faire un cadeau est-elle une fille ?");
+    }, 1000);
   }, []);
 
   // filtrer les cadeaux en fonction des réponses
@@ -76,14 +95,12 @@ const Form = () => {
     const chat = document.getElementById('chat');
     const bubbleChat = document.querySelector('.chat-box');
     const chatFooter = document.querySelector('.chat-footer');
+
+
+
+    
   
   // Fonction pour ajouter un message à la zone de chat
-
-
-  
-  
-  
-  
   const addMessage = (text, sender) => {
     const message = document.createElement('div');
     message.classList.add(`${sender}-message`);
@@ -95,9 +112,7 @@ const Form = () => {
     bubbleChat.scrollTop = bubbleChat.scrollHeight;
   }
   
-  
-  
-  function hideButton(classe) {
+  const hideButton = (classe) => {
     // Récupération de tous les éléments ayant la même classe
     const elements = document.getElementsByClassName(classe);
     
@@ -107,28 +122,29 @@ const Form = () => {
     }
   }
 
-  const createButtonWithFunction = (onClickFunction) => {
-    const button1 = document.createElement("button");
-    const button2 = document.createElement("button");
-    button1.innerHTML = 'Oui';
-    button1.onclick = onClickFunction;
-    button2.innerHTML = 'Non';
-    button2.onclick = onClickFunction;
-    chatFooter.appendChild(button1);
-    chatFooter.appendChild(button2);
+  const showButton = (classe) => {
+    // Récupération de tous les éléments ayant la même classe
+    const elements = document.getElementsByClassName(classe);
+    
+    // Affichage des éléments
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.display = "block";
+    }
   }
-  
-  
+
+
   const woman = () => {
-    let botText = "D'accord, et est-ce un adulte?";
-    let userText = "Oui c'est une femme";
-    const classe = "firstBtn";
-    const onClickFunction = "blabla"
-    hideButton(classe)
-    createButtonWithFunction(onClickFunction);
+    let userText = "Oui c'est une fille";
+    let botText = "D'accord, et est-ce une adulte?";
+    const genderButton = "genderButton";
+    const adultButton = "adultButton";
+
+    hideButton(genderButton)
+    showButton(adultButton)
     
     setPath('1');
     setGender('woman');
+
     setTimeout(() => {
       addMessage(userText, user);
     }, "500");
@@ -139,15 +155,143 @@ const Form = () => {
   }
 
 
-
-
   const man = () => {
+    let userText = "Non c'est un garçon";
     let botText = "D'accord, et est-ce un adulte?";
-    let userText = "Oui c'est une homme";
-    const classe = "firstBtn";
-    const onClickFunction = "blabla"
-    hideButton(classe)
-    createButtonWithFunction(onClickFunction);
+    const genderButton = "genderButton";
+    const adultButton = "adultButton";
+
+    hideButton(genderButton)
+    showButton(adultButton)
+
+    
+    setPath('0');
+    setGender('man');
+
+    setTimeout(() => {
+      addMessage(userText, user);
+    }, "500");
+
+    setTimeout(() => {
+      addMessage(botText, bot);
+    }, "2000");
+  }
+
+
+  const adult = () => {
+    let userText = "Oui c'est une adulte";
+    let botText = "D'accord, et est-ce un membre de votre famille??";
+    const adultButton = "adultButton";
+    const familleButton = "familleButton";
+    hideButton(adultButton)
+    showButton(familleButton)
+
+    setAge('adulte');
+    if (path === "1") {
+      setPath('1,0');
+    }
+    setTimeout(() => {
+      addMessage(userText, user);
+    }, "500");
+
+    setTimeout(() => {
+      addMessage(botText, bot);
+    }, "2000");
+  }
+
+  const notAdult = () => {
+    let userText = "Non il n'est pas adulte";
+    let botText = "D'accord, et est-ce un ado?";
+    const adultButton = "adultButton";
+    const familleButton = "familleButton"
+    hideButton(adultButton)
+    showButton(familleButton)
+    
+    if (path === "1") {
+      setPath('1,1');
+    }
+    setTimeout(() => {
+      addMessage(userText, user);
+    }, "500");
+
+    setTimeout(() => {
+      addMessage(botText, bot);
+    }, "2000");
+  }
+
+  const ado = () => {
+    let userText = "Oui c'est un ado";
+    let botText = "D'accord, et est-ce un membre de votre famille?";
+    const adoButton = "adoButton";
+    const familleButton = "familleButton"
+    setAge('ado');
+
+    hideButton(adoButton)
+    showButton(familleButton)
+    
+    if (path === "1") {
+      setPath('1,1');
+    }
+    setTimeout(() => {
+      addMessage(userText, user);
+    }, "500");
+
+    setTimeout(() => {
+      addMessage(botText, bot);
+    }, "2000");
+  }
+
+  const notAdo = () => {
+    let userText = "Non c'est un enfant";
+    let botText = "D'accord, et est-ce un membre de votre famille?";
+    const adoButton = "adoButton";
+    const familleButton = "familleButton"
+
+    setAge('enfant');
+
+    hideButton(adoButton)
+    showButton(familleButton)
+
+    
+    if (path === "1") {
+      setPath('1,2');
+    }
+    setTimeout(() => {
+      addMessage(userText, user);
+    }, "500");
+
+    setTimeout(() => {
+      addMessage(botText, bot);
+    }, "2000");
+  }
+
+
+  const famille = () => {
+    let userText = "Oui c'est un membre de ma famille";
+    let botText = "D'accord, et quels sont ces centres d'interets?";
+    const familleButton = "familleButton";
+    // const familleButton = "familleButton"
+    hideButton(familleButton)
+    // showButton(familleButton)
+    
+    setPath('0');
+    setGender('man');
+    setTimeout(() => {
+      addMessage(userText, user);
+    }, "500");
+
+    setTimeout(() => {
+      addMessage(botText, bot);
+    }, "2000");
+  }
+
+  const notFamille = () => {
+    let userText = "Non ce n'est pas un memmbre de ma famille";
+    let botText = "D'accord, et est-ce un ami?";
+    const familleButton = "familleButton"
+    // const adoButton = "adoButton";
+    hideButton(familleButton)
+    // showButton(familleButton)
     
     setPath('0');
     setGender('man');
@@ -161,6 +305,10 @@ const Form = () => {
   }
 
 
+
+
+
+
   return (
     <>
       <div className="chat-container">
@@ -170,19 +318,35 @@ const Form = () => {
         <div className="chat-box">
           <div className="chat" id="chat">
             <div className="bot-message fade-in">
-              <p>Bonjour, je m'apelle Ruby je viens t'aider à trouver un cadeau parceque t'es une merde !</p>
+            {firstMessage && <p>{firstMessage}</p>}             
+
+            
             </div>
             <div className="bot-message fade-in">
-              <p>Pose ton petit cul et admire</p>
+              
+              {secondMessage && <p>{secondMessage}</p>}             
+
             </div>
             <div className="bot-message fade-in">
-              <p>Tout d'abord, la personne à qui tu veux faire un cadeau est-elle une femme ?</p>
+            {thirdMessage && <p>{thirdMessage}</p>}             
             </div>
           </div>
         </div>
         <div className="chat-footer">
-          <button id='woman' className='firstBtn' onClick={woman}>Oui</button>
-          <button id='man' className='firstBtn' onClick={man}  >Non</button>
+          <button id='woman' className='genderButton' onClick={woman}>Oui</button>
+          <button id='man' className='genderButton' onClick={man}  >Non</button>
+
+          <button id='adult' className='adultButton' onClick={adult}>Oui</button>
+          <button id='not-adult' className='adultButton' onClick={notAdult}>Non</button>
+
+          <button id='ado' className='adoButton' onClick={ado}>Oui</button>
+          <button id='not-ado' className='adoButton' onClick={notAdo}>Non</button>
+
+          <button id='famille' className='familleButton' onClick={famille}>Oui</button>
+          <button id='not-famille' className='familleButton' onClick={notFamille}>Non</button>
+
+          {/* <button id='collegue' className='collegueButton' onClick={collegue}>Oui</button>
+          <button id='not-collegue' className='collegueButton' onClick={notCollegue}>Non</button> */}
         </div>
       </div>
     </>
